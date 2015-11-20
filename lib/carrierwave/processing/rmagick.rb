a9ee246e9121bc2d8da1d91a90a77885955315ff
Tony Noecker <noecker@gmail.com>
2015-11-20 13:47:07 -0600

    require rmagick instead of RMagick

--- lib/carrierwave/processing/rmagick.rb
+++ lib/carrierwave/processing/rmagick.rb
@@ -62,7 +62,7 @@ module CarrierWave
 
     included do
       begin
-        require "RMagick"
+        require "rmagick"
       rescue LoadError => e
         e.message << " (You may need to install the rmagick gem)"
         raise e
